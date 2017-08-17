857a7b0fffb7f0477b5c3d17073da0ef88b08c65
jakenielson <groakayt@gmail.com>
2017-08-17 14:50:54 -0700

    Conform priority button to project style

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,7 +19,7 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
-			'click .priority-btn': 'priority',
+			'click .priority-btn': 'togglePriority',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
 			'keypress .edit': 'updateOnEnter',
@@ -52,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -70,6 +71,11 @@ var app = app || {};
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
+		},
+
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
@@ -124,11 +130,6 @@ var app = app || {};
 				// Also reset the hidden input back to the original value.
 				this.$input.val(this.model.get('title'));
 			}
-		},
-
-		// Highlight the item in red
-		priority: function () {
-			this.$el.toggleClass('priority');
 		},
 
 		// Remove the item, destroy the model from *localStorage* and delete its view.
